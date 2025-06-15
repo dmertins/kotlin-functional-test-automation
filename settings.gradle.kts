737ca6d51396e8634fb8e04e0f87c7c9ed97d26f
Daniel Alves Mertins <dalvesmertins@gmail.com>
2025-06-15 20:00:50 -0300

    Create Gradle Project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "kotlin-functional-test-automation"
+
+include(
+    "acceptance-tests",
+    "api-tests",
+    "mobile-tests",
+    "playwright-web-tests",
+    "selenium-web-tests",
+)
