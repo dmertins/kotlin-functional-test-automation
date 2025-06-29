10c912017c507d026a865d2ceefc529911b4d19a
Daniel Alves Mertins <dalvesmertins@gmail.com>
2025-06-29 19:20:14 -0300

    Add REST Assured dependencies

--- api-tests/build.gradle.kts
+++ api-tests/build.gradle.kts
@@ -1,3 +1,19 @@
 plugins {
     id("kotlin-conventions")
 }
+
+dependencies {
+    testImplementation(platform("io.rest-assured:rest-assured-bom:5.5.5"))
+    testImplementation("io.rest-assured:rest-assured")
+    testImplementation("io.rest-assured:kotlin-extensions")
+    testImplementation("io.rest-assured:json-schema-validator")
+
+    constraints {
+        testImplementation("commons-codec:commons-codec:1.13") {
+            because("versions before 1.13 are vulnerable")
+        }
+        testImplementation("com.fasterxml.jackson.core:jackson-core:2.15.0") {
+            because("versions before 2.15.0 are vulnerable")
+        }
+    }
+}
